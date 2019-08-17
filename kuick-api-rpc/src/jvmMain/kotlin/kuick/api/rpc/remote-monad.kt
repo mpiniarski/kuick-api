a1f981187393e25370db1f8ca965f8728cc0549d
mpiniarski <piniarski.m@gmail.com>
2019-08-17 12:01:13 +0200

    Strong Remote Monad POC

--- kuick-api-rpc/src/jvmMain/kotlin/kuick/api/rpc/remote-monad.kt
+++ kuick-api-rpc/src/jvmMain/kotlin/kuick/api/rpc/remote-monad.kt
@@ -1,15 +1,19 @@
 package kuick.api.rpc
 
 import arrow.Kind
-import arrow.core.ForId
 import arrow.core.Id
+import arrow.core.Tuple2
 import arrow.core.extensions.id.monad.monad
 import arrow.core.fix
-import arrow.data.ForKleisli
+import arrow.data.ForStateT
 import arrow.data.Reader
-import arrow.data.extensions.kleisli.monad.binding
+import arrow.data.ReaderPartialOf
+import arrow.data.StateT
+import arrow.data.extensions.kleisli.applicative.applicative
+import arrow.data.extensions.kleisli.monad.monad
+import arrow.data.extensions.statet.monad.binding
 import arrow.data.fix
-import arrow.data.flatMap
+import arrow.data.run
 import arrow.typeclasses.MonadContinuation
 import kuick.api.core.buildArgsFromArray
 import kuick.api.core.toJson
@@ -21,20 +25,27 @@ import kotlin.reflect.KFunction
 import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.jvm.jvmName
 
-typealias Remote<T> = Reader<Device, T?>
+// typealias Remote<T> = ReaderT<ForState, Device, State<List<Command<T>>, T?>>
+typealias Remote<T> = StateT<ReaderPartialOf<Device>, List<Command<T>>, T?>
 
-private inline fun <reified T : Any> sendCommand(command: Command<T>): Remote<T> = Remote<T> { device ->
-    if (T::class.isInstance(Unit)) {
-        device.async(command.serialize())
-        Id(null)
-    } else {
-        Id(
-            Json.fromJson(
-                device.sync(command.serialize())
-            )
-        )
+private inline fun <reified T : Any> sendCommand(command: Command<T>): Remote<T> =
+    StateT<ReaderPartialOf<Device>, List<Command<T>>, T?>(Reader.applicative(Id.monad())) { commands ->
+        Reader { device ->
+            if (T::class.isInstance(Unit)) {
+                // device.async(command.serialize())
+                Id(Tuple2(commands + command, null))
+            } else {
+                Id(
+                    Tuple2(
+                        emptyList(),
+                        Json.fromJson(
+                            device.sync((commands + command).serialize())
+                        )
+                    )
+                )
+            }
+        }
     }
-}
 
 class Command<T>(
     val serviceClass: KClass<out Any>,
@@ -57,18 +68,35 @@ class Command<T>(
     }
 }
 
+private const val COMMAND_SEPARATOR = "///"
+
+fun <T> List<Command<T>>.serialize() = this.joinToString(separator = COMMAND_SEPARATOR) { it.serialize() }
+
 interface Device {
-    fun async(command: String): Unit
-    fun sync(command: String): String
+    fun async(packet: String): Unit
+    fun sync(packet: String): String
 }
 
-fun <T> send(device: Device, remote: Remote<T>): T? = remote.run(device).fix().extract()
+fun <T> send(device: Device, remote: Remote<T>): T? {
+    val (commands, result) = remote.run(Reader.monad(Id.monad()), emptyList())
+        .run(device)
+        .fix().extract()
+    return if (commands.isNotEmpty()) {
+        device.async(commands.serialize())
+        null
+    } else {
+        result
+    }
+}
 
 fun <T> send(
     device: Device,
-    block: suspend MonadContinuation<Kind<Kind<ForKleisli, ForId>, Device>, *>.() -> T
+    block: suspend MonadContinuation<Kind<Kind<ForStateT, ReaderPartialOf<Device>>, List<Command<*>>>, *>.() -> T
 ): T? =
-    send(device, binding(Id.monad(), block).fix() as Remote<T>)
+    send(
+        device,
+        binding<ReaderPartialOf<Device>, List<Command<*>>, T?>(Reader.monad(Id.monad()), block).fix() as Remote<T>
+    )
 
 fun <T : Any> getInstance(clazz: KClass<T>): T =
     when (clazz) {
@@ -76,20 +104,30 @@ fun <T : Any> getInstance(clazz: KClass<T>): T =
         else -> throw RuntimeException("Service not found")
     } as T
 
-class MockDevice : Device {
-    override fun sync(command: String): String {
-        val command = Command.deserialize<Any>(command)
-        val api = getInstance(command.serviceClass)
-        return Json.toJson(
+class MockDevice :
+    Device { // normally device should send over http and on server side there should be a way to deserialize and handle
+    override fun sync(packet: String): String =
+        handleCommands(packet)
+            .last()
+            .let { Json.toJson(it) }
+
+    override fun async(packet: String): Unit {
+        handlePacket(packet) { command ->
+            val api = getInstance(command.serviceClass)
             command.method.call(api, *command.params.toTypedArray())
-        )
+        }
     }
 
-    override fun async(command: String): Unit {
-        val command = Command.deserialize<Any>(command)
-        val api = getInstance(command.serviceClass)
-        command.method.call(api, *command.params.toTypedArray())
-    }
+    private fun handlePacket(command: String, handleCommand: (Command<Any>) -> Any): List<Any> =
+        command.split(COMMAND_SEPARATOR)
+            .map { Command.deserialize<Any>(it) }
+            .map(handleCommand)
+
+    private fun handleCommands(packet: String): List<Any> =
+        handlePacket(packet) { command ->
+            val api = getInstance(command.serviceClass)
+            command.method.call(api, *command.params.toTypedArray())
+        }
 }
 
 data class Resource(
@@ -112,41 +150,28 @@ open class ResourceApiClient {
     fun printSmth(s: String): Remote<Unit> = sendCommand(Command(this::class, ResourceApi::printSmth, listOf(s)))
 }
 
-class RemoteMonadResourceApiClient(private val api: ResourceApiClient) {
-    suspend fun MonadContinuation<Kind<Kind<ForKleisli, ForId>, Device>, *>.getOne(): Resource? =
-        bind { api.getOne() }
-
-    suspend fun MonadContinuation<Kind<Kind<ForKleisli, ForId>, Device>, *>.printSmth(s: String) {
-        bind { api.printSmth(s) }
-    }
-}
+// class RemoteMonadResourceApiClient(private val api: ResourceApiClient) {
+//     suspend fun MonadContinuation<Kind<Kind<ForKleisli, ForId>, Device>, *>.getOne(): Resource? =
+//         bind { api.getOne() }
+//
+//     suspend fun MonadContinuation<Kind<Kind<ForKleisli, ForId>, Device>, *>.printSmth(s: String) {
+//         bind { api.printSmth(s) }
+//     }
+// }
 
 suspend fun main() {
     val device = MockDevice()
 
     val api = ResourceApiClient()
 
-    send(device,
-        api.printSmth("1").flatMap {
-            api.printSmth("2")
-        }
-    )
-
-    println()
-    println()
-
-    println(
-        send(device, api.getOne())
-    )
-
-    println()
-    println()
+    send(device, api.printSmth("test"))
+    println(send(device, api.getOne()))
 
     println(
         send(device) {
-            val (a) = api.getOne()
             bind { api.printSmth("1") }
-            a
+            bind { api.printSmth("2") }
+            bind { api.printSmth("3") }
         }
     )
 }
